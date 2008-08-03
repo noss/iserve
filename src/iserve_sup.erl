205eda18b631532cd2596d13c310f964cb58dfaa
Christian Sunesson <chsu79@gmail.com>
2008-08-03 02:38:21 +0200

    Able to start iserve with iserve_sup:start_link/0,2,3 to be used in your own supervision tree

--- src/iserve_sup.erl
+++ src/iserve_sup.erl
@@ -2,6 +2,8 @@
 -behaviour(supervisor).
 -export([
 	 start_link/0,
+	 start_link/2,
+	 start_link/3,
          init/1
         ]).
 
@@ -9,9 +11,15 @@
 
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+start_link(Port, Callback) ->
+    supervisor:start_link(?MODULE, [Port, Callback]).
+start_link(Port, Callback, CallbackData) ->
+    supervisor:start_link(?MODULE, [Port, Callback, CallbackData]).
 
 init([]) ->
     ConfigArgs = get_config(),
+    init(ConfigArgs);
+init(ConfigArgs) ->
     Server = {iserve_server, 
 	      {iserve_server, start_link, ConfigArgs},
 	      permanent, 2000, worker, [iserve_server]},
